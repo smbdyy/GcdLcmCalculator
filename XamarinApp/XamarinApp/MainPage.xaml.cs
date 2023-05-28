3e9652d1ce2fdb0b3621269e8fdee3b3e1e7e37a
Ilya <ividivi67@gmail.com>
2023-05-28 21:37:56 +0300

    feat: add xamarin app

---
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(NumberAEntry.Text, out var number1) && int.TryParse(NumberBEntry.Text, out var number2))
+            {
+                ResultLabel.Text = $"Введенные числа: {number1}, {number2}";
+            }
+            else
+            {
+                ResultLabel.Text = "Ошибка: введите два целых числа.";
+            }
+        }
+    }
+}
