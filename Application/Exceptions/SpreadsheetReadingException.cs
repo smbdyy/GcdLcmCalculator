6d1cecfff85d43904ee2939a9634b6f409d172a6
Ilya <ividivi67@gmail.com>
2023-05-28 13:16:26 +0300

    feat: add spreadsheet reading

---
+++ Application/Exceptions/SpreadsheetReadingException.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Exceptions;
+
+public class SpreadsheetReadingException : ApplicationException
+{
+    public SpreadsheetReadingException(string? message)
+        : base(message) { }
+
+    public SpreadsheetReadingException()
+        : base("Ошибка при чтении таблицы") { }
+}
---
+++ Application/MicrosoftOfficeTools/Spreadsheets.cs
@@ -0,0 +1,59 @@
+﻿using Application.Exceptions;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace Application.MicrosoftOfficeTools;
+
+public static class Spreadsheets
+{
+    public static Tuple<int, int> ReadTwoNumbersFromTable(string filePath)
+    {
+        using var document = SpreadsheetDocument.Open(filePath, false);
+        WorkbookPart? workbookPart = document.WorkbookPart;
+
+        if (workbookPart == null)
+        {
+            throw new SpreadsheetReadingException();
+        }
+
+        WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+        Worksheet worksheet = worksheetPart.Worksheet;
+        SheetData? sheetData = worksheet.GetFirstChild<SheetData>();
+
+        if (sheetData == null)
+        {
+            throw new SpreadsheetReadingException();
+        }
+
+        int a = GetCellValue(sheetData, "A1");
+        int b = GetCellValue(sheetData, "A2");
+
+        return new Tuple<int, int>(a, b);
+    }
+
+    public static int GetCellValue(SheetData sheetData, string cellReference)
+    {
+        Cell cell = GetCell(sheetData, cellReference);
+        if (cell.DataType != null && cell.DataType.Value == CellValues.Number && cell.CellValue != null)
+        {
+            return int.Parse(cell.CellValue.Text);
+        }
+        return 0;
+    }
+
+    public static Cell GetCell(SheetData sheetData, string cellReference)
+    {
+        foreach (Row row in sheetData.Elements<Row>())
+        {
+            foreach (Cell cell in row.Elements<Cell>())
+            {
+                if (cell.CellReference?.Value == cellReference)
+                {
+                    return cell;
+                }
+            }
+        }
+
+        throw new SpreadsheetReadingException();
+    }
+}
