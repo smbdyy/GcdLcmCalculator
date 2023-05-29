e8b9f6ee820ee787730e3d7ec1a44dacc17bc87c
Ilya <ividivi67@gmail.com>
2023-05-29 19:10:58 +0300

    feat: add separate calculation button & loading data from table

---
+++ XamarinApp/XamarinApp/Constants/FilePickerFileTypes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Xamarin.Essentials;
+
+namespace XamarinApp.Constants;
+
+public static class FilePickerFileTypes
+{
+    public static FilePickerFileType SpreadsheetType { get; }
+        = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+        {
+            { DevicePlatform.iOS, new[] { "com.microsoft.excel.xlsx" } },
+            { DevicePlatform.Android, new[] { "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" } }
+        });
+
+    public static FilePickerFileType WordDocumentType { get; }
+        = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+        {
+            { DevicePlatform.iOS, new[] { "org.openxmlformats.wordprocessingml.document" } },
+            { DevicePlatform.Android, new[] { "application/vnd.openxmlformats-officedocument.wordprocessingml.document" } }
+        });
+}
--- XamarinApp/XamarinApp/MainPage.xaml.cs
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using GcdLcmCalculatorApplication.MicrosoftOfficeTools;
+using GcdLcmCalculatorApplication.Models;
 using GcdLcmCalculatorApplication.Service;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using XamarinApp.Constants;
 
 namespace XamarinApp
 {
@@ -11,17 +15,69 @@ namespace XamarinApp
             InitializeComponent();
         }
 
-        private async void CalculateButton_Clicked(object sender, EventArgs e)
+        private async void ShowSolutionButton_Clicked(object sender, EventArgs e)
         {
             ResultLabel.Text = "";
-            if (int.TryParse(NumberAEntry.Text, out var numberA) && int.TryParse(NumberBEntry.Text, out var numberB)
-                                                                 && numberA > 0 && numberB > 0)
+            if (TryParseNumbers(out var numberA, out var numberB))
             {
                 await Navigation.PushAsync(new ResultsPage(GcdLcmCalculator.GetCalculationSteps(numberA, numberB)));
             }
             else
             {
                 ResultLabel.Text = "Ошибка: введите два натуральных числа.";
+            }
+        }
+
+        private void CalculateButton_Clicked(object sender, EventArgs e)
+        {
+            ResultLabel.Text = "";
+            if (TryParseNumbers(out var numberA, out var numberB))
+            {
+                GcdLcmValues values = GcdLcmCalculator.CalculateValues(numberA, numberB);
+                ResultLabel.Text = $"НОД: {values.Gcd}, НОК: {values.Lcm}";
+            }
+            else
+            {
+                ResultLabel.Text = "Ошибка: введите два натуральных числа.";
+            }
+        }
+
+        private bool TryParseNumbers(out int numberA, out int numberB)
+        {
+            if (int.TryParse(NumberAEntry.Text, out var number1) && int.TryParse(NumberBEntry.Text, out var number2)
+                                                                 && number1 > 0 && number2 > 0)
+            {
+                numberA = number1;
+                numberB = number2;
+                return true;
+            }
+
+            numberA = 1;
+            numberB = 1;
+            return false;
+        }
+
+        private async void LoadDataFromTable_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var options = new PickOptions
+                {
+                    FileTypes = FilePickerFileTypes.SpreadsheetType
+                };
+
+                FileResult? fileResult = await FilePicker.PickAsync(options);
+
+                if (fileResult == null) return;
+                string filePath = fileResult.FullPath;
+                Tuple<int, int> numbers = Spreadsheets.ReadTwoNumbersFromTable(filePath);
+
+                NumberAEntry.Text = $"{numbers.Item1}";
+                NumberBEntry.Text = $"{numbers.Item2}";
+            }
+            catch (Exception ex)
+            {
+                ResultLabel.Text = "Произошла ошибка при открытии файла.";
             }
         }
     }
