47636b1ac95472fc324ad149c51c08cb127b3f06
Ilya <ividivi67@gmail.com>
2023-05-30 17:35:15 +0300

    feat: add instructions resource

---
+++ GcdLcmCalculatorApplication/Resources/Abstractions/ResourceLoader.cs
@@ -0,0 +1,6 @@
+﻿namespace GcdLcmCalculatorApplication.Resources.Abstractions;
+
+public static class ResourceLoader
+{
+    private static string Instrctions { get; } = Resources.Instructions.Replace("\n", Environment.NewLine);
+}
